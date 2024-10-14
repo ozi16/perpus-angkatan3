929c5b20f399fece5a89c9ae7e2dca341da8e6dc
ozi16 <muhammadfahrurozi1603@gmail.com>
2024-10-14 14:37:47 +0700

    second commit

--- home.php
+++ home.php
@@ -9,7 +9,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Permanent+Marker&display=swap" rel="stylesheet" />
 
-  <title>rumah</title>
+  <title>Home</title>
 </head>
 
 <body style="font-family: Indie Flower; font-weight: bold">
